db5727b29917b0b7134759fc7d2ec0014fba4fd9
onomatope2580 <79963946+onomatope2580@users.noreply.github.com>
2021-03-12 11:25:59 +0900

    AjaxAppの準備

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,16 @@
 class PostsController < ApplicationController
+  
+  def index
+    @posts = Post.order(id: "DESC")
+  end
+
+   # コメントアウトする
+  # def new
+  # end
+
+  def create
+    Post.create(content: params[:content])
+    redirect_to action: :index
+  end
+
 end
